516a86f44e8fc9093a810eeddfbd5425bc7a8e57
barrezyari2909 <149530308+barrezyari2909@users.noreply.github.com>
2026-03-17 03:49:44 -0500

    Add files via upload

--- webserver.c
+++ webserver.c
@@ -10,11 +10,15 @@ int main(int argc, char **argv) {
 	if(argc > 1) port = atoi(argv[1]);
 
 	int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(server_fd < 0){	
+		perror("socket error");
+		exit(1);
+	}
 	struct sockaddr_in addr;
 	addr.sin_family = AF_INET; addr.sin_addr.s_addr = INADDR_ANY; addr.sin_port = htons(port);
 	bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
 	listen(server_fd, 5);
-	printf("luisteren op port ", port);
+	printf("luisteren op port %d\n", port);
 	
 	while(1) {
 		int client = accept(server_fd, NULL, NULL);
